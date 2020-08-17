40782e3ead2d682f3cf6002fa90e45b9fa04d83a
holgerengels <hengels@gmail.com>
2020-08-17 12:31:13 +0200

    o build problems o remove js and js.map for ts

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,7 +1,11 @@
 import { createSpaConfig } from '@open-wc/building-rollup';
 import merge from 'deepmerge';
 
-const baseConfig = createSpaConfig();
+const baseConfig = createSpaConfig({
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  injectServiceWorker: false,
+    }
+);
 
 export default merge(baseConfig, {
   input: './index.html'
