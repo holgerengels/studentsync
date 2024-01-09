314a057cf27c2ec83bfd4284b2f12da77a52ceac
holgerengels <hengels@gmail.com>
2024-01-09 22:03:36 +0100

    chore(ssl): try without extended trust manager

--- server/src/main/java/studentsync/domains/Schulkonsole.java
+++ server/src/main/java/studentsync/domains/Schulkonsole.java
@@ -44,9 +44,8 @@ public class Schulkonsole
 
     static ThreadLocal<PoolingHttpClientConnectionManager> connectionManager = ThreadLocal.withInitial(() -> {
         // ExtendedTrustManager.getInstance(Configuration.getInstance().getConfig());
-        return PoolingHttpClientConnectionManagerBuilder.create().build();
+        //return PoolingHttpClientConnectionManagerBuilder.create().build();
 
-        /*
         try {
             SSLContext sslContext = SSLContext.getInstance("TLSv1.3");
             sslContext.init(null, new TrustManager[]{ExtendedTrustManager.INSTANCE}, new SecureRandom());
@@ -61,7 +60,6 @@ public class Schulkonsole
         catch (NoSuchAlgorithmException | KeyManagementException e) {
             throw new RuntimeException(e);
         }
-        */
     });
 
     ThreadLocal<CloseableHttpClient> client = ThreadLocal.withInitial(() -> HttpClientBuilder.create().setConnectionManager(connectionManager.get()).build());
